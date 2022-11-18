0a990b6e654518c64ee6b04ce5f314343bc3e0cc
smori <shinichiro.mori.1983@gmail.com>
2022-11-18 23:27:54 +0900

    Init vuepress config

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  base: '/peg-study-demo/',
+  title: 'peg-study-demo',
+  dest: 'docs/.vuepress/dist',
+
+  themeConfig: {
+    search: false,
+    nav: [
+      { text: 'GitHub', link: 'https://github.com/smori1983/peg-study-demo' },
+    ],
+    sidebar: [
+    ],
+  },
+
+  plugins: [
+  ],
+
+  markdown: {
+    extendMarkdown: (md) => {
+      md.set({ breaks: true });
+    },
+  },
+};

7f2831c258141e5920f599b1a7dbee2770ddefba
smori <shinichiro.mori.1983@gmail.com>
2022-12-23 00:39:20 +0900

    Rename components

--- docs/.vuepress/plugins/peg/enhanceAppFile.js
+++ docs/.vuepress/plugins/peg/enhanceAppFile.js
@@ -1,9 +1,9 @@
 import Calc01 from './components/Calc01';
-import Digest01 from './components/Digest01';
-import Variable01 from './components/Variable01';
+import CalcDigest01 from './components/CalcDigest01';
+import CalcVariable01 from './components/CalcVariable01';
 
 export default ({Vue}) => {
   Vue.component('CalcCalc01', Calc01);
-  Vue.component('CalcDigest01', Digest01);
-  Vue.component('CalcVariable01', Variable01);
+  Vue.component('CalcDigest01', CalcDigest01);
+  Vue.component('CalcVariable01', CalcVariable01);
 }

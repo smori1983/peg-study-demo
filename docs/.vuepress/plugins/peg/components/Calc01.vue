8bccdea744ae89b61051b236f710bde634f642e1
smori <shinichiro.mori.1983@gmail.com>
2022-12-31 10:14:30 +0900

    Extract styles

--- docs/.vuepress/plugins/peg/components/Calc01.vue
+++ docs/.vuepress/plugins/peg/components/Calc01.vue
@@ -6,7 +6,7 @@
       </div>
       <div class="box">
         <div class="label">Final expression</div>
-        <div class="infix-notation">{{ infixNotation }}</div>
+        <div class="result">{{ infixNotation }}</div>
       </div>
     </div>
 
@@ -16,7 +16,7 @@
       @changed="tabChanged"
     >
       <tab name="Graph">
-        <div class="svg">
+        <div class="side-by-side">
           <div class="left">
             <div class="label">Original</div>
             <svg class="ast-graph" ref="ast-01"></svg>
@@ -116,90 +116,5 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
-.top {
-  .box {
-    margin-bottom 1rem
-  }
-  .label {
-    margin-bottom 0.5rem
-    font-weight bold
-  }
-  .input {
-    min-width 100%
-    max-width 100%
-    min-height 3rem
-  }
-  .infix-notation {
-    min-height 2rem
-    font-family monospace
-  }
-}
-.tabs-component {
-  margin 0
-  width 100%
-  >>> .tabs-component-tabs {
-    margin 0 0 1rem
-    padding 0
-    align-items stretch
-    display flex
-    justify-content flex-start
-    .tabs-component-tab {
-      margin 0
-      padding 0
-      color #999
-      font-size 14px
-      font-weight 600
-      list-style none
-      .tabs-component-tab-a {
-        margin 0
-        padding 0 2rem 0 0
-        line-height 2.5rem
-        align-items center
-        color inherit
-        display flex
-        text-decoration none
-      }
-    }
-    .tabs-component-tab:hover {
-      color #666
-    }
-    .tabs-component-tab.is-active {
-      color #000
-    }
-  }
-  >>> .tabs-component-panels {
-    margin 0
-    padding 0
-  }
-}
-.svg {
-  display flex
-  .left {
-    width 50%
-  }
-  .right {
-    width 50%
-  }
-  .label {
-    margin-bottom 10px
-    font-weight bold
-  }
-  .ast-graph {
-    margin 0 1px
-    width calc(100% - 4px)
-    height 400px
-    border 1px solid black
-
-    >>> .link {
-      stroke gray
-      stroke-width 0.5px
-    }
-  }
-}
-.ast {
-  pre {
-    background-color lightgray
-    color black
-  }
-}
+@import "style/common.styl"
 </style>
